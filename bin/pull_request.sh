8c4fce3cbdc99a511415c7891bf5f9c6dbbe153d
jogendra <jogendra.kumar@zomato.com>
2020-05-17 08:48:10 +0530

    Add git aliases

---
+++ bin/pull_request.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function _zpr() {
+    current_branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
+    parent_branch_with_remote=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
+    parent_branch="${parent_branch_with_remote##*/}"
+    github_username=$(git config user.name)
+    remote_url=$(git ls-remote --get-url)
+    base_url=$(dirname "$remote_url")
+    repo_name=$(basename -s .git "$remote_url")
+    full_url="$base_url/$repo_name"
+    zprURL="$full_url/compare/$parent_branch...$github_username:$current_branch?expand=1"
+    open $zprURL
+}
+_zpr
---
+++ xcode/aliases.zsh
@@ -0,0 +1,2 @@
+alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
+alias watchos="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator\ \(Watch\).app"
