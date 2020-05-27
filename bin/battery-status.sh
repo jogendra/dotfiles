a5e3bcf5b67033504ecd6ceee45d32ba44816bae
jogendra <imjog24@gmail.com>
2020-05-28 02:24:10 +0530

    Add battery status script

---
+++ bin/battery-status.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# battery-status
+#
+# A quick little indicator for battery status on your Mac laptop, suitable for
+# display in your prompt.
+
+function battery_status() {
+    if test ! "$(uname)" = "Darwin"
+    then
+    printf ""
+    exit 0
+    fi
+
+    battstat=$(pmset -g batt | grep -Eo "\d+%")
+
+    time_left=$(pmset -g batt |
+    tail -1 |
+    cut -f2 |
+    awk -F"; " '{print $3}' |
+    cut -d' ' -f1
+    )
+
+    if [[ $(pmset -g ac) == *"No adapter attached."* ]]
+    then
+    emoji='🔋'
+    else
+    emoji='🔌'
+    fi
+
+    timeleft_emoji='⌛'
+
+    if [[ $time_left == *"(no"* || $time_left == *"not"* ]]
+    then
+    time_left='⌛️'
+    fi
+
+    if [[ $time_left == *"0:00"* ]]
+    then
+    time_left='⚡️ '
+    fi
+
+    echo "$emoji $battstat $timeleft_emoji $time_left"
+}
+battery_status
