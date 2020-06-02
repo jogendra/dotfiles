fdff9531b40fa1fe5d1f78cdf0d8a50bd9e7d5ba
jogendra <imjog24@gmail.com>
2020-06-03 02:52:35 +0530

    Add a xcode alias

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,2 +1,3 @@
 alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
 alias watchos="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator\ \(Watch\).app"
+alias rmdd="rm -rf ~/Library/Developer/Xcode/DerivedData"
